43138c7de4173fad2f965b12948f898af5d6a8fd
Thuathe <raifuteika15@gmail.com>
2025-07-25 21:52:55 +0700

    home v1.5 alamat

--- src/components/LoginRegister/api/axiosClient.js
+++ src/components/LoginRegister/api/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: 'https://invanorbackend-production.up.railway.app/api/v1',
 });
-
+// https://invanorbackend-production.up.railway.app/
 export default axiosClient;
